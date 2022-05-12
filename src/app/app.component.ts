f8b6aff24e02d40ed69bdcb072ef32321844da5f
Mateus Rauli <mateus.rauli@gmail.com>
2022-05-11 21:50:01 -0300

    generating a random password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,30 +12,49 @@ export class AppComponent {
   includeNumbers = false;
   length = 0;
 
-  useLetters(){
-      this.includeLetters = true;
+  useLetters() {
+    this.includeLetters = true;
   }
 
-  useNumbers(){
+  useNumbers() {
     this.includeNumbers = true;
   }
 
-  useSymbols(){
+  useSymbols() {
     this.includeSymbols = true;
   }
-  onButtonClick(){
-    console.log(this.includeLetters);
-    console.log(this.includeNumbers);
-    console.log(this.includeSymbols);
-    this.password = 'My Password';
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random()*validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 
-  changeLength(value: string){
-      const parsedValue = parseInt(value);
-      if(!isNaN(parsedValue)){
-        this.length = parsedValue;
-      }
+  changeLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
   }
 
-  
+
 }  
